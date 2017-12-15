896854312c51cb303cf0bd187b294655bc88074d
Doug Fales <doug@getharvest.com>
2017-12-14 22:44:31 -0700

    I think this is doomed to fail because MacVim only receives the visible bytes from vim, not the entire file.

---
+++ src/MacVim/MVPPreviewView.h
@@ -0,0 +1,15 @@
+//
+//  MVPPreviewView.h
+//  MacVim
+//
+//  Created by Doug on 12/14/17.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "MMWindowController.h"
+
+@interface MVPPreviewView : NSView
+
+@property (nonatomic, retain) MMWindowController *windowController;
+
+@end
---
+++ src/MacVim/MVPPreviewView.m
@@ -0,0 +1,22 @@
+//
+//  MVPPreviewView.m
+//  MacVim
+//
+//  Created by Doug on 12/14/17.
+//
+
+#import "MVPPreviewView.h"
+#import "MMTextView.h"
+
+@implementation MVPPreviewView
+
+- (void)drawRect:(NSRect)dirtyRect {
+ 
+    if(self.windowController) {
+        MMTextView *tv = (MMTextView *)[[self.windowController vimView] textView];
+        [tv displayRectIgnoringOpacity:dirtyRect inContext:[NSGraphicsContext currentContext]];
+    }
+    
+}
+
+@end
