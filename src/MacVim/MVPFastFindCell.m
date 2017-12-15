d17dd31c9716adaa89ef67e94ce620e184f7d697
Doug Fales <doug@getharvest.com>
2017-12-15 06:23:04 -0700

    Cleaning up some drawing code and making the fast find foreground white when selected, so it is readable.

--- src/MacVim/MVPFastFindCell.m
+++ src/MacVim/MVPFastFindCell.m
@@ -11,11 +11,6 @@
 #import "MVPProject.h"
 #import "MVPFastFindResult.h"
 
-@interface MVPFastFindCell () 
--(NSAttributedString *)addAttributesToString:(NSString *)filename;
-@end
-
-
 @implementation MVPFastFindCell
 
 @synthesize fastFindController;
@@ -31,51 +26,67 @@
 	[super dealloc];
 }
 
-- (void)drawWithFrame:(NSRect)cellFrame inView:(NSView *)controlView {
-	
-	MVPFastFindResult *item = [self objectValue];
-	NSAttributedString *aString = [self addAttributesToString:[item title]];
-	[aString drawAtPoint:NSMakePoint(cellFrame.origin.x + 28, cellFrame.origin.y + 4)];
-	
-	
-	NSColor *gray = [NSColor grayColor];
-	NSDictionary *pathAttributes = [NSDictionary dictionaryWithObjectsAndKeys:gray, NSForegroundColorAttributeName, [NSFont systemFontOfSize:9], NSFontAttributeName, nil];
+- (void)drawTitle:(NSRect)cellFrame {
+    NSAttributedString *title = [self addFilenameAttributes:[[self objectValue] title]];
+    [title drawAtPoint:NSMakePoint(cellFrame.origin.x + 28, cellFrame.origin.y + 4)];
+}
 
-	[[item name] drawAtPoint:NSMakePoint(cellFrame.origin.x + 28, cellFrame.origin.y + 21) withAttributes:pathAttributes];
-	
-	[[NSGraphicsContext currentContext] saveGraphicsState];
-	
-	
-	float startY = cellFrame.origin.y;
-	if([controlView isFlipped]) {
-		NSAffineTransform *transform = [NSAffineTransform transform];
-		[transform translateXBy:0.0 yBy:cellFrame.size.height];
-		[transform scaleXBy:1.0 yBy:-1.0];
-		[transform concat];
-		startY = -cellFrame.origin.y;
-	}
-	
-    NSImage *icon =  [item icon];
+- (void)drawPath:(NSRect)cellFrame {
+    NSString *path = [[self objectValue] name];
+    [path drawAtPoint:NSMakePoint(cellFrame.origin.x + 28, cellFrame.origin.y + 21) withAttributes:[self pathAttributes]];
+}
+
+- (void)drawIcon:(NSRect)cellFrame inView:(NSView *)controlView {
+    float startY = cellFrame.origin.y;
+    if([controlView isFlipped]) {
+        NSAffineTransform *transform = [NSAffineTransform transform];
+        [transform translateXBy:0.0 yBy:cellFrame.size.height];
+        [transform scaleXBy:1.0 yBy:-1.0];
+        [transform concat];
+        startY = -cellFrame.origin.y;
+    }
     
-	NSRect toRect = NSMakeRect(cellFrame.origin.x +2, startY + 3, 25, 25);
-	NSRect fromRect = NSMakeRect(0, 0, [icon size].width, [icon size].height);
-	NSImageInterpolation interp = [[NSGraphicsContext currentContext] imageInterpolation];
-	[[NSGraphicsContext currentContext] setImageInterpolation:NSImageInterpolationHigh];
-	[icon drawInRect:toRect fromRect:fromRect operation:NSCompositingOperationSourceOver fraction:1.0];
+    NSImage *icon = [[self objectValue] icon];
+    NSRect toRect = NSMakeRect(cellFrame.origin.x +2, startY + 3, 25, 25);
+    NSRect fromRect = NSMakeRect(0, 0, [icon size].width, [icon size].height);
+    NSImageInterpolation interp = [[NSGraphicsContext currentContext] imageInterpolation];
+    [[NSGraphicsContext currentContext] setImageInterpolation:NSImageInterpolationHigh];
+    [icon drawInRect:toRect fromRect:fromRect operation:NSCompositingOperationSourceOver fraction:1.0];
+    [[NSGraphicsContext currentContext] setImageInterpolation:interp]; // Not part of graphics state; won't be reset by restoreGraphicsState
+}
+
+- (void)drawWithFrame:(NSRect)cellFrame inView:(NSView *)controlView {
+
+    [[NSGraphicsContext currentContext] saveGraphicsState];
+
+    [self drawTitle:cellFrame];
 	
-	[[NSGraphicsContext currentContext] setImageInterpolation:interp];
+    [self drawPath:cellFrame];
+	
+    [self drawIcon:cellFrame inView:controlView];
+
 	[[NSGraphicsContext currentContext] restoreGraphicsState];
 	
 }
 
--(NSAttributedString *)addAttributesToString:(NSString *)filename  {
-	NSColor *filenameColor = ([self isHighlighted] ? [NSColor alternateSelectedControlTextColor] : [NSColor grayColor]);	
-	NSDictionary *filenameAttributes = [NSDictionary dictionaryWithObjectsAndKeys:filenameColor, NSForegroundColorAttributeName, [NSFont systemFontOfSize:14], NSFontAttributeName, nil];
+- (NSColor *)textColor {
+    return ([self isHighlighted] ? [NSColor alternateSelectedControlTextColor] : [NSColor grayColor]);
+}
+
+-(NSAttributedString *)addFilenameAttributes:(NSString *)filename  {
+	NSDictionary *filenameAttributes = [NSDictionary dictionaryWithObjectsAndKeys:[self textColor], NSForegroundColorAttributeName, [NSFont systemFontOfSize:14], NSFontAttributeName, nil];
 	NSMutableAttributedString *aString = [[NSMutableAttributedString alloc] initWithString:filename attributes:filenameAttributes]; 
     NSRange r =	[filename rangeOfString:[fastFindController searchString]];
     [aString addAttribute:NSFontAttributeName value:[NSFont boldSystemFontOfSize:14] range:r];
-	[aString addAttribute:NSForegroundColorAttributeName value:[NSColor blackColor] range:r];
+    NSColor *searchStringHighlightColor = ([self isHighlighted] ? [NSColor alternateSelectedControlTextColor] : [NSColor blackColor]);
+	[aString addAttribute:NSForegroundColorAttributeName value:searchStringHighlightColor range:r];
     return aString;    
+}
+
+-(NSDictionary *)pathAttributes  {
+    return [NSDictionary dictionaryWithObjectsAndKeys:
+            [self textColor], NSForegroundColorAttributeName,
+            [NSFont systemFontOfSize:9], NSFontAttributeName, nil];
 }
 
 @end
